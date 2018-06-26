aacfeca89b3c27e3c703b97d53f2ad5b0cbee805
TamaraAlhajj <tamara.alhajj@carleton.ca>
2018-06-25 22:28:21 -0400

    Login keyboard avoiding not working, will fix

--- ProjectFabric/App.js
+++ ProjectFabric/App.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //import { StackNavigator, } from 'react-navigation';
 import Splash from './Splash';
-//import Login from './src/components/Login/Login';
+import Login from './src/components/login/Login';
 
 export default class App extends Component {
   render() {
     return (
-      <Splash/>
-      //<Login/>
+      //<Splash/>
+      <Login/>
     );
   }
 }
 
 /**const App = StackNavigator({
   SplashPage: {screen: Splash},
+  LandingPage: {screen: Landing},
   LoginPage: {screen: Login},
 });**/
--- ProjectFabric/Splash.js
+++ ProjectFabric/Splash.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+//TODO: image looks bad and is not aligned, fix with bkgrd color
 
 export default class Splash extends Component {
     render() {
         return (
-            <View style={styles.wrapper}>
-                <Text style={styles.title}>BuildUp</Text>
-                <Text style={styles.subtitle}>Powered by React Native</Text>
+            <View style={styles.content}>
+                <View style={styles.wrapper}>
+                    <Image 
+                    style={styles.logo}
+                    source={require('./src/components/images/shopify-logo.png')} 
+                    />
+                    <Text style={styles.title}>BuildUp</Text>
+                </View>
+                    <Text style={styles.subtitle}>Powered by React Native</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        backgroundColor: 'blue',
+    content: {
+        backgroundColor: 'white',
         flex: 1, 
         justifyContent: 'center',
         alignItems: 'center'
     },
+    logo: {
+        resizeMode:'contain',
+        width: 100,
+        height: 100
+    },
     title: {
-        color: 'white',
+        color: 'green',
         fontSize:40, 
         fontWeight: 'bold'
     },
     subtitle: {
-        color: 'white',
-        fontWeight: '200'
+        color: 'green',
+        fontSize: 15,
+        fontWeight: '100'
+    },
+    wrapper:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1
     }
 });
